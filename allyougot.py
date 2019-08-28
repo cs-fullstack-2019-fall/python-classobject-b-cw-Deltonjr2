6c83f43400583bee11e9108fe8a30878fddd7aa4
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-28 12:59:39 -0500

    class

---
+++ allyougot.py
@@ -0,0 +1,75 @@
+# python-classObject_b-cw
+# Start with a main function and make each problem a function. Call those functions from your main function.
+#
+# Problem 1:
+#
+# Create a class Dog. Make sure it has the attributes name, breed, color, gender. Create a function that will print all attributes of the class. Create an object of Dog in your problem1 function and print all of it's attributes.
+#
+# Problem 2:
+#
+# We will keep having this problem until EVERYONE gets it right without help
+#
+# Create a function that has a loop that quits with the equal sign. If the user doesn't enter the equal sign, ask them to input another string.
+#
+# Problem 3:
+#
+# Create a class Product that represents a product sold online. A product has a name, price, and quantity.
+#
+# The class should have several functions:
+#
+# a) One function that can change the name of the product.
+#
+#     b) Another function that can change the name and price of the product.
+#
+#     c) A last function that can change the name, price, and quantity of the product.
+#
+#     Create an object of Product and print all of it's attributes.
+
+# 1. # Create a class Dog. Make sure it has the attributes name, breed, color, gender. Create a function that will print all attributes of the class. Create an object of Dog in your problem1 function and print all of it's attributes.
+
+class Dog:
+    def __init__(self, name, breed, color, gender):
+# my properties
+        self.name=name
+        self.breed=breed
+        self.color=color
+        self.gender=gender
+
+    def ShowmyWork(self):
+
+        print(f"{self.name}, {self.breed}, {self.color},{self.gender}")
+
+def problem1():
+    dogOption1= Dog("fido","bulldog", "black", "male")
+    dogOption1.ShowmyWork()
+
+problem1()
+
+# //2. Create a function that has a loop that quits with the equal sign. If the user doesn't enter the equal sign, ask them to input another string.
+
+userInput = ""
+while(userInput != "="):
+    userInput = input("Enter another string")
+
+//3. # Create a class Product that represents a product sold online. A product has a name, price, and quantity.
+#
+# The class should have several functions:
+#
+# a) One function that can change the name of the product.
+#     b) Another function that can change the name and price of the product.
+#
+#     c) A last function that can change the name, price, and quantity of the product.
+#
+#     Create an object of Product and print all of it's attributes.
+
+class Plasticplates():
+
+def __init__(self, plasticplate,75.00, 500):
+
+    self.name=name
+    self.price=price
+    self.quantity=quantity
+
+ def mywork(self):
+     print(f"{self.name}, {self.price}, {self.quantity},")
+
